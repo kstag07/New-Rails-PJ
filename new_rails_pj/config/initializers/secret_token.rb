9c4d0d6c0f7017f32a260d636c497b18181c4de0
Kevin Staiger <kstag07@yahoo.com>
2014-02-11 19:35:09 -0800

    Adding a new rails application new_rails_pj

---
+++ new_rails_pj/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewRailsPj::Application.config.secret_key_base = 'ddf086fd97f40ca4bdaf9174e4627b2cbd1835ea57e797ac7528be9f111e85e9f28d0063d46ca695732e91f1332b5379ab56de228fedca8ed15105100c8abba1'
