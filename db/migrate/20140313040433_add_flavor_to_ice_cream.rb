1984be633c4f5b4eba6924a8dd9f66045fd64d86
Kevin Staiger <kstag07@yahoo.com>
2014-03-26 19:13:37 -0700

    final project

--- db/migrate/20140313040433_add_flavor_to_ice_cream.rb
+++
@@ -1,5 +0,0 @@
-class AddFlavorToIceCream < ActiveRecord::Migration
-  def change
-    add_column :ice_creams, :flavor, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140320032356) do
+ActiveRecord::Schema.define(version: 20140326051229) do
 
   create_table "ice_creams", force: true do |t|
     t.integer  "store_id"
